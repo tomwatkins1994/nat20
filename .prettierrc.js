0913153049bb8e46ca61022a82e2c551bd9ead83
Tom Watkins <twwatkins@btinternet.com>
2020-08-29 01:17:16 +0100

    Config files

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    tabWidth: 4,
+    bracketSpacing: true,
+    semi: true,
+    arrowParens: 'avoid',
+};
