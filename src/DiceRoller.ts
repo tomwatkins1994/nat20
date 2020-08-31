9e50c7bd65f0bdd73b1f490b75809c4b27b1cce1
Tom Watkins <twwatkins@btinternet.com>
2020-08-31 22:34:34 +0100

    Restructuring of class

--- src/DiceRoller.ts
+++ src/DiceRoller.ts
@@ -1,7 +1,15 @@
 import d from './utils/d';
 import * as math from 'mathjs';
 
-interface DiceRoller {
+class DiceRoller {
+    private rolls: number[];
+    private resultText: string;
+
+    constructor() {
+        this.rolls = [];
+        this.resultText = null;
+    }
+
     roll(
         rollText: string,
         callback?: (
@@ -12,83 +20,71 @@ interface DiceRoller {
             },
             error?: string,
         ) => any,
-    ): number;
-    getDiceRolls(): number[];
-    getResultText(): string;
-}
-
-class DiceRoller {
-    constructor() {
-        // Private props
-        let rolls = [];
-        let resultText = null;
-
-        this.roll = (rollText, callback) => {
-            try {
-                rollText = rollText.toLowerCase();
-                let invalidCharacters = rollText.match(/(?![dD])[A-Za-z]+/g);
-                if (invalidCharacters)
-                    throw new Error(
-                        `Invalid characters entered: ${invalidCharacters.join(
-                            ', ',
-                        )}`,
+    ) {
+        try {
+            rollText = rollText.toLowerCase();
+            let invalidCharacters = rollText.match(/(?![dD])[A-Za-z]+/g);
+            if (invalidCharacters)
+                throw new Error(
+                    `Invalid characters entered: ${invalidCharacters.join(
+                        ', ',
+                    )}`,
+                );
+            let allRolls = [];
+            let resultExpression = rollText;
+            let resultDisplayText = rollText;
+            rollText
+                .match(/(?:[0-9]+d)([0-9]+)|(?:d)([0-9]+)/g)
+                .forEach(die => {
+                    let numRolls =
+                        parseInt(
+                            (die.match(/([0-9]+)(?=d)/g) || '').toString(),
+                        ) || 1;
+                    let maxRoll = parseInt(
+                        (die.match(/(?<=d)([0-9]+)/g) || '').toString(),
                     );
-                let allRolls = [];
-                let resultExpression = rollText;
-                let resultDisplayText = rollText;
-                rollText
-                    .match(/(?:[0-9]+d)([0-9]+)|(?:d)([0-9]+)/g)
-                    .forEach(die => {
-                        let numRolls =
-                            parseInt(
-                                (die.match(/([0-9]+)(?=d)/g) || '').toString(),
-                            ) || 1;
-                        let maxRoll = parseInt(
-                            (die.match(/(?<=d)([0-9]+)/g) || '').toString(),
-                        );
-                        let rolls = [];
-                        let total = 0;
-                        for (let i = 1; i <= numRolls; i++) {
-                            let result = d(maxRoll);
-                            total += result;
-                            rolls.push(result);
-                            allRolls.push(result);
-                        }
-                        resultExpression = resultExpression.replace(
-                            die,
-                            `(${total})`,
-                        );
-                        resultDisplayText = resultDisplayText.replace(
-                            die,
-                            `[${rolls.join(',')}]`,
-                        );
-                    });
-                const diceRollResult = math.evaluate(resultExpression);
-                rolls = allRolls;
-                resultText = `${rollText} = ${resultDisplayText} = ${diceRollResult}`;
-                if (typeof callback === 'function')
-                    callback({
-                        value: diceRollResult,
-                        rolls: allRolls,
-                        resultText: resultText,
-                    });
-                return diceRollResult;
-            } catch (err) {
-                rolls = [];
-                resultText = `${rollText} - Failed to perform dice roll - ${err}`;
-                if (typeof callback === 'function')
-                    callback({ value: 0, rolls: [], resultText: null }, err);
-                return 0;
-            }
-        };
+                    let rolls = [];
+                    let total = 0;
+                    for (let i = 1; i <= numRolls; i++) {
+                        let result = d(maxRoll);
+                        total += result;
+                        rolls.push(result);
+                        allRolls.push(result);
+                    }
+                    resultExpression = resultExpression.replace(
+                        die,
+                        `(${total})`,
+                    );
+                    resultDisplayText = resultDisplayText.replace(
+                        die,
+                        `[${rolls.join(',')}]`,
+                    );
+                });
+            const diceRollResult = math.evaluate(resultExpression);
+            this.rolls = allRolls;
+            this.resultText = `${rollText} = ${resultDisplayText} = ${diceRollResult}`;
+            if (typeof callback === 'function')
+                callback({
+                    value: diceRollResult,
+                    rolls: allRolls,
+                    resultText: this.resultText,
+                });
+            return diceRollResult;
+        } catch (err) {
+            this.rolls = [];
+            this.resultText = `${rollText} - Failed to perform dice roll - ${err}`;
+            if (typeof callback === 'function')
+                callback({ value: 0, rolls: [], resultText: null }, err);
+            return 0;
+        }
+    }
 
-        this.getDiceRolls = () => {
-            return rolls;
-        };
+    getDiceRolls() {
+        return this.rolls;
+    }
 
-        this.getResultText = () => {
-            return resultText;
-        };
+    getResultText() {
+        return this.resultText;
     }
 }
 
