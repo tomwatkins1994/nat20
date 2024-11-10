313669afee4b7954881775ea7cd48c74a1397127
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2024-11-10 20:15:54 +0000

    Tidied up tests and handled error cases

--- src/dice-roller.test.ts
+++ src/dice-roller.test.ts
@@ -1,50 +1,41 @@
-import d from '../src/utils/d';
-import DiceRoller from '../src/DiceRoller';
-import { expect } from 'chai';
-import 'mocha';
-import addContext from 'mochawesome/addContext';
+import { DiceRoller } from './dice-roller';
+import { describe, expect, test } from 'vitest';
 
 describe('DiceRoller = Simple Tests', function () {
     const diceRoller = new DiceRoller();
-    it(`2d6 - Result should be between 2 and 12`, function () {
+    test(`2d6 - Result should be between 2 and 12`, function () {
         diceRoller.roll('2d6', (result, error) => {
-            if (error) addContext(this, error);
-            else addContext(this, result.resultText);
+            expect(error).toBeUndefined();
             expect(result.value).to.be.gte(2).and.lte(12);
         });
     });
-    it(`3d6 - Result should be between 3 and 18`, function () {
+    test(`3d6 - Result should be between 3 and 18`, function () {
         diceRoller.roll('3d6', (result, error) => {
-            if (error) addContext(this, error);
-            else addContext(this, result.resultText);
+            expect(error).toBeUndefined();
             expect(result.value).to.be.gte(3).and.lte(18);
         });
     });
-    it(`2d6+10 - Result should be between 12 and 22`, function () {
+    test(`2d6+10 - Result should be between 12 and 22`, function () {
         diceRoller.roll('2d6+10', (result, error) => {
-            if (error) addContext(this, error);
-            else addContext(this, result.resultText);
+            expect(error).toBeUndefined();
             expect(result.value).to.be.gte(12).and.lte(22);
         });
     });
-    it(`3d6+10 - Result should be between 13 and 28`, function () {
+    test(`3d6+10 - Result should be between 13 and 28`, function () {
         diceRoller.roll('3d6+10', (result, error) => {
-            if (error) addContext(this, error);
-            else addContext(this, result.resultText);
+            expect(error).toBeUndefined();
             expect(result.value).to.be.gte(13).and.lte(28);
         });
     });
-    it(`2d6+d10 - Result should be between 3 and 22`, function () {
+    test(`2d6+d10 - Result should be between 3 and 22`, function () {
         diceRoller.roll('2d6+d10', (result, error) => {
-            if (error) addContext(this, error);
-            else addContext(this, result.resultText);
+            expect(error).toBeUndefined();
             expect(result.value).to.be.gte(3).and.lte(22);
         });
     });
-    it(`3D6+d10 - Result should be between 4 and 28`, function () {
+    test(`3D6+d10 - Result should be between 4 and 28`, function () {
         diceRoller.roll('3d6+d10', (result, error) => {
-            if (error) addContext(this, error);
-            else addContext(this, result.resultText);
+            expect(error).toBeUndefined();
             expect(result.value).to.be.gte(4).and.lte(28);
         });
     });
@@ -52,10 +43,10 @@ describe('DiceRoller = Simple Tests', function () {
 
 describe('DiceRoller - Error Handling', function () {
     const diceRoller = new DiceRoller();
-    it(`2x6 - Result should be an error due to invalid character`, function () {
-        diceRoller.roll('2x6', (result, error) => {
-            addContext(this, error.toString());
-            expect(error.toString()).length.is.greaterThan(0);
+    test(`2x6 - Result should be an error due to invalid character`, function () {
+        diceRoller.roll('2x6', (_, error) => {
+            expect(error).toBeDefined();
+            expect(error).length.is.greaterThan(0);
         });
     });
 });
